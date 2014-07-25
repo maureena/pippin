251de41073d42740f8601c1d235a9ac9d19b7142
vagrant <vagrant@rails-dev-box>
2014-07-25 05:33:19 +0000

    Created parent relationship for subevents

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
+  belongs_to :parent, class_name: 'Event'
+  has_many :events, foreign_key: :parent_id
   has_and_belongs_to_many :venues
   has_and_belongs_to_many :categories
 end
